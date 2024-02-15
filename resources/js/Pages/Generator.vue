1b67cdf77c99e7dd33c71f52306d3e73536e1d54
Anne van der Aar <annevanderaar@hotmail.com>
2024-02-15 17:00:39 +0100

    added base for generator

---
+++ resources/js/Pages/Generator.vue
@@ -0,0 +1,24 @@
+<template>
+  <PageHeader :title="$t('generator')"/>
+
+  <h1 class="text-center">
+    {{ $t('generator') }}
+  </h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import PageHeader from '@/Components/PageHeader.vue';
+
+export default defineComponent({
+  name: 'Generator',
+
+  components: {
+    PageHeader,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>

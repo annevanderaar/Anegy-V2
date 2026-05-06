ea29de7225b9a90ab9ae426bc92c70f8acafc3c8
Anne van der Aar <annevanderaar@hotmail.com>
2026-05-06 14:59:42 +0200

    Small change to the watched migration.

--- database/migrations/2024_09_01_110115_create_watched_table.php
+++ database/migrations/2024_09_01_110115_create_watched_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('watcheds', function (Blueprint $table) {
+        Schema::create('watched', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
             $table->integer('ms_id');
